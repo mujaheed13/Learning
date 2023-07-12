0eaeec9b149d293fbe2d2a106637c8d8ef2d3f1f
mujaheed13 <mujaheedsami13@gmail.com>
2023-07-12 17:39:39 +0530

    useState & conditional rendering

---
+++ react/react-app/src/components/Alert.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface AlertProps {
+  onClose: () => void;
+}
+
+const Alert = ({ onClose }: AlertProps) => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>Holy guacamole!</strong> You should check in on some of those
+      fields below.
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
--- react/react-app/src/components/ListGroup.tsx
+++ react/react-app/src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
+import Alert from "./Alert";
 
-interface Props { 
+interface Props {
   items: string[];
   heading: string;
-  onSelectItem: () => void
+  onSelectItem: (item: string) => void;
 }
 
-const ListGroup = ({items, heading, onSelectItem}: Props) => {
-
+const ListGroup = ({ items, heading, onSelectItem }: Props) => {
   const [selected_i, setSelected_i] = useState(-1);
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <>
@@ -17,15 +18,33 @@ const ListGroup = ({items, heading, onSelectItem}: Props) => {
       <ul className="list-group">
         {items?.map((item, i) => {
           return (
-            <li key={item} className={i==selected_i ? "list-group-item active": "list-group-item" } onClick={()=>{
-              setSelected_i(i);
-              onSelectItem(item)
-            }}>
+            <li
+              key={item}
+              className={
+                i == selected_i ? "list-group-item active" : "list-group-item"
+              }
+              onClick={() => {
+                setSelected_i(i);
+                onSelectItem(item);
+              }}
+            >
               {item}
             </li>
           );
         })}
       </ul>
+      {alertVisible && <Alert onClose={()=>{
+        setAlertVisibility(false);
+      }}/>}
+      <button
+        type="button"
+        className="btn  btn-primary"
+        onClick={() => {
+          setAlertVisibility(true);
+        }}
+      >
+        Base class
+      </button>
     </>
   );
 };
